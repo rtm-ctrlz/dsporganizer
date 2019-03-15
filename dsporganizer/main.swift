f7c2fb68a0364292f4e70973b366c0812deea731
Ilia Urvachev <rtm@ctrlz.ru>
2019-03-15 13:53:36 +0300

    Initial Commit

---
+++ dsporganizer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  dsporganizer
+//
+//  Created by Внештатный командир земли on 15.03.2019.
+//  Copyright © 2019 Внештатный командир земли. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
